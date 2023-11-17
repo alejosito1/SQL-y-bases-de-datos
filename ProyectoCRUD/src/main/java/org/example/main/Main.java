7f9fd40b6777d305b710de6251dca1c643eeda1f
Sergio Torres <sergio.t94.mez@gmail.com>
2023-11-16 21:00:41 -0500

    Visualizando bases de datos con Java Swing

--- ProyectoCRUD/src/main/java/org/example/main/Main.java
+++ ProyectoCRUD/src/main/java/org/example/main/Main.java
@@ -1,36 +1,11 @@
 package org.example.main;
 
-import org.example.model.Employee;
-import org.example.repository.EmployeeRepository;
-import org.example.repository.Repository;
-import org.example.util.DatabaseConnection;
-
-import java.sql.*;
+import org.example.view.SwingApp;
 
 public class Main {
 
-    public static void main(String[] args) throws SQLException {
-        try(Connection myConn = DatabaseConnection.getInstance()){
-            Repository<Employee> repository = new EmployeeRepository();
-
-            System.out.println("---Listando---");
-            repository.findAll().forEach(System.out::println);
-
-//            System.out.println("---Actualizando un empleado---");
-//            Employee employee = new Employee();
-//            employee.setId(4);
-//            employee.setFirstName("Diego");
-//            employee.setPaSurName("Pimentel");
-//            employee.setMaSurName("Villanueva");
-//            employee.setEmail("diego@gmail.com");
-//            employee.setSalary(30000F);
-//            repository.save(employee);
-
-            System.out.println("---Empleado eliminado---");
-            repository.delete(4);
-
-            System.out.println("---Listando---");
-            repository.findAll().forEach(System.out::println);
-        }
+    public static void main(String[] args){
+        SwingApp app = new SwingApp();
+        app.setVisible(true);
     }
 }
