cead03c8be3902a26da782c4e1664202cb42e2b3
Sergio Torres <sergio.t94.mez@gmail.com>
2023-12-13 18:03:07 -0500

    Implementando conexiones pool

--- ProyectoCRUD/src/main/java/org/example/main/Main.java
+++ ProyectoCRUD/src/main/java/org/example/main/Main.java
@@ -11,37 +11,12 @@ import java.sql.SQLException;
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-        try(Connection myConn = DatabaseConnection.getInstance()) {
 
-            if(myConn.getAutoCommit()){
-                myConn.setAutoCommit(false);
-            }
+        System.out.println("----Listando todos ----");
+        Repository<Employee> repository = new EmployeeRepository();
+        repository.findAll().forEach(System.out::println);
 
-            try {
-                Repository<Employee> repository = new EmployeeRepository(myConn);
-                System.out.println("-------Insertar un nuevo cliente-------");
-                Employee employee = new Employee();
-                /*employee.setFirstName("America");
-                employee.setPaSurName("Lopez");
-                employee.setMaSurName("Villa");
-                employee.setEmail("ame2@example.com");
-                employee.setSalary(3000F);
-                employee.setCurp("AMEC14257894EDFRT3");
-                repository.save(employee);
-                myConn.commit();*/
-
-                employee.setFirstName("David");
-                employee.setPaSurName("Gutierrez");
-                employee.setMaSurName("Olvera");
-                employee.setEmail("olv2@example.com");
-                employee.setSalary(3000F);
-                employee.setCurp("AMEC14257894EDFRT3");
-                repository.save(employee);
-                myConn.commit();
-            } catch (SQLException e) {
-                myConn.rollback();
-                throw new RuntimeException(e);
-            }
-        }
+        System.out.println("----Buscando por ID----");
+        System.out.println(repository.getById(2));
     }
 }
--- ProyectoCRUD/src/main/java/org/example/util/DatabaseConnection.java
+++ ProyectoCRUD/src/main/java/org/example/util/DatabaseConnection.java
@@ -1,5 +1,7 @@
 package org.example.util;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,13 +10,25 @@ public class DatabaseConnection {
     private static String url = "jdbc:mysql://localhost:3306/employees";
     private static String user = "root";
     private static String pass = "";
-    private static Connection myConn;
+    private static BasicDataSource pool;
 
-    public static Connection getInstance() throws SQLException {
-        if(myConn == null){
-            myConn = DriverManager.getConnection(url,user,pass);
+    public static BasicDataSource getInstance() throws SQLException {
+        if(pool == null){
+            pool = new BasicDataSource();
+            pool.setUrl(url);
+            pool.setUsername(user);
+            pool.setPassword(pass);
+
+            pool.setInitialSize(3);
+            pool.setMinIdle(3);
+            pool.setMaxIdle(10);
+            pool.setMaxTotal(10);
         }
-        return myConn;
+        return pool;
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return getInstance().getConnection();
     }
 
 }
